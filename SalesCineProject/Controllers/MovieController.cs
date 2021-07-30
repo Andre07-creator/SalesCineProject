2934ba50c3e6ab12ed7e5a2da6869821ff5c61dc
Andre07-creator <aramaldes876@gmail.com>
2021-07-30 11:01:48 -0300

    Contoroll Index e View criadas e configuradas

---
+++ SalesCineProject/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesCineProject.Models;
+using SalesCineProject.Services;
+
+namespace SalesCineProject.Controllers
+{
+    public class MovieController : Controller
+    {
+        public readonly MovieService _movieservice;
+
+        public MovieController(MovieService movieservice)
+        {
+            _movieservice = movieservice;
+        }
+
+        public IActionResult Index()
+        {
+            var List = _movieservice.FindAll();
+            return View(List);
+        }
+    }
+}
